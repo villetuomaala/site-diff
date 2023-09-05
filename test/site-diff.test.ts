bb95026454c7583bad13099b04abb902133f9508
villetuomaala <ville.tuomaala@almamedia.fi>
2023-09-05 21:31:33 +0300

    cdk init

---
+++ test/site-diff.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SiteDiff from '../lib/site-diff-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/site-diff-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SiteDiff.SiteDiffStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
